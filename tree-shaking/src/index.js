fb6935c8017eede49c7d5eab8b614698534481eb
yamachan0625 <napoli.080625@gmail.com>
2021-03-29 00:45:14 +0900

    Production settings

---
+++ tree-shaking/src/index.js
@@ -0,0 +1,14 @@
+import { cube, square } from './math';
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
+
+function component() {
+  const element = document.createElement('pre');
+  element.innerHTML = ['Hello webpack', '5 cubed is equal to' + cube(5)].join(
+    '\n\n'
+  );
+  return element;
+}
+document.body.appendChild(component());
